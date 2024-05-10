5d88cdc7ae1c0e29e5a782872a40af5c077132da
unknown <kalungi.roberts2000@gmail.com>
2024-05-10 20:44:44 +0200

    first commit

--- Src/main.js
+++
@@ -1,8 +0,0 @@
-const btnEL = document.querySelector(".btn");
-
-btnEL.addEventListener("mouseover",(event) => {
-    const x =(event.pageX - btnEL.offsetLeft);
-    const y =(event.pagey - btnEL.offsetTop);
-
-    btnEL.Style.setproperty("--xPos", x + "px");
-}); btnEL.Style.setproperty("--yPos", y + "px");
